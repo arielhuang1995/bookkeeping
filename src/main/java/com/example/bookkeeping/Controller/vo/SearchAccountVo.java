54cf24be734be6f500f7294f62e56b5f604813df
nick <trojanhorse0077@gmail.com>
2021-02-26 09:44:58 +0800

    adjust SearchAccountVo default startDate and endDate

--- src/main/java/com/example/bookkeeping/Controller/vo/SearchAccountVo.java
+++ src/main/java/com/example/bookkeeping/Controller/vo/SearchAccountVo.java
@@ -7,8 +7,8 @@ import java.time.LocalDateTime;
 @Data
 public class SearchAccountVo {
 
-    private LocalDateTime startDate = LocalDateTime.now();
-    private LocalDateTime endDate = this.getStartDate().plusMonths(1);
+    private LocalDateTime startDate = LocalDateTime.of(LocalDateTime.now().getYear(), LocalDateTime.now().getMonth(), 1, 0, 0);
+    private LocalDateTime endDate = this.getStartDate().plusMonths(1).minusDays(1);
     private String keyWord4Item;
 
 }
--- src/test/java/com/example/bookkeeping/CalendarTests.java
+++ src/test/java/com/example/bookkeeping/CalendarTests.java
@@ -4,29 +4,39 @@ package com.example.bookkeeping;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDateTime;
-import java.util.Calendar;
-import java.util.Date;
 
 
 public class CalendarTests {
 
     @Test
     public void testStartDateAndEndDate(){
-        Calendar calendar = Calendar.getInstance();
-        calendar.setTimeInMillis(new Date().getTime());
-        calendar.set(Calendar.DAY_OF_MONTH, 1);
+//        Calendar calendar = Calendar.getInstance();
+//        calendar.setTimeInMillis(new Date().getTime());
+//        calendar.set(Calendar.DAY_OF_MONTH, 1);
+//
+//        System.out.println();
+//        System.out.println(calendar.getTime());
+//
+//        calendar.set(Calendar.DAY_OF_MONTH, calendar.getActualMaximum(Calendar.DAY_OF_MONTH));
+//        System.out.println(calendar.getTime());
+//        System.out.println(calendar.getTime());
+//
+//        LocalDateTime d1 = LocalDateTime.now();
+//        LocalDateTime d2 = d1.plusMonths(1);
+//
+//
+//        System.out.println(d1);
+//        System.out.println(d2);
 
-        System.out.println();
-        System.out.println(calendar.getTime());
+        LocalDateTime now = LocalDateTime.now();
 
-        calendar.set(Calendar.DAY_OF_MONTH, calendar.getActualMaximum(Calendar.DAY_OF_MONTH));
-        System.out.println(calendar.getTime());
-        System.out.println(calendar.getTime());
+        LocalDateTime d3 = LocalDateTime.of(now.getYear(), now.getMonth(), 1, 0, 0);
+        LocalDateTime d4 = d3.plusMonths(1).minusDays(1);
 
-        LocalDateTime d1 = LocalDateTime.now();
-        LocalDateTime d2 = d1.plusMonths(1);
 
-        System.out.println(d1);
-        System.out.println(d2);
+
+
+        System.out.println(d3);
+        System.out.println(d4);
     }
 }
